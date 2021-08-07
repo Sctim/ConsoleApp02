0b81525b0acace7c9d8f1fab650907d5d4fd2e6c
Sctim <amori5798@gmail.com>
2021-08-07 15:16:40 +0430

    create project

---
+++ ConsoleApp02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 7; i++)
+            {
+                Console.WriteLine("".PadRight(i, '*'));
+            }
+        }
+    }
+}
